ceb3535aa0cd632b111c5f9aae9a5993024426fe
admin123 <admin>
2018-11-27 08:42:24 +0530

    head revisioin

---
+++ Demo_Git_Jenkins/src/test/java/com/MyTest.java
@@ -0,0 +1,26 @@
+package com;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+import org.junit.Test;
+
+public class MyTest {
+
+@Test
+public void myTest()
+{
+	String name="hello";
+	assertEquals(5, name.length());
+	
+}
+
+@Test
+public void myTest1()
+{
+	String name="hello";
+	assertNotEquals(4, name.length());
+	
+}
+
+
+}
